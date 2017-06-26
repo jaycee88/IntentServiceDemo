4d8f88c40ba572b22fc555158c11080135c416bd
jaycee <6453862@qq.com>
2017-06-26 19:17:23 +0800

    unregister receiver

--- app/src/main/java/com/jaycee88/intentservicedemo/MainActivity.java
+++ app/src/main/java/com/jaycee88/intentservicedemo/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyBackgroundTaskReceiver localReceiver;
+
     private static final String ACTION_RESULT = "com.jaycee88.intentservicedemo.action.RESULT";
 
     @Override
@@ -15,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         IntentFilter intentFilter = new IntentFilter(ACTION_RESULT);
-        MyBackgroundTaskReceiver receiver = new MyBackgroundTaskReceiver();
-        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
+        localReceiver = new MyBackgroundTaskReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(localReceiver, intentFilter);
 
 //        MyBackgroundTaskIntentService.startIntentService(this);
         ForeService.startService(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(localReceiver);
     }
 }
